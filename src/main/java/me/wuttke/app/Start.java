e6a593b801858fbe89e55143dcca3075194d0cd6
Andreas Wuttke <a.wuttke@outlook.com>
2020-07-31 13:57:08 +0200

    inital version

---
+++ src/main/java/me/wuttke/app/Start.java
@@ -0,0 +1,7 @@
+package me.wuttke.app;
+
+public class Start {
+    public static void main(String[] args) {
+        System.out.println("Hello Java 14!");
+    }
+}
---
+++ src/test/java/me/wuttke/app/StartTest.java
@@ -0,0 +1,14 @@
+package me.wuttke.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class StartTest {
+
+    @Test
+    public void HelloJunit5() {
+        assertEquals("Hallo", "Hallo");
+
+    }
+
+}
